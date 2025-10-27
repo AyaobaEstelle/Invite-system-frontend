b7f13008dfd59582f0c3dcaab238674ae8a50b19
AyaobaEstelle <abolarinesther98@gmail.com>
2025-10-27 16:57:30 +0100

    Removed old route

--- src/app/admin/invites/[id]/page.tsx
+++ src/app/admin/invites/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Layout } from "@/components/admin/dashboard/Layout";
+import { DashboardLayout } from "@/components/layout/dashboard/Layout";
 import Spinner from "@/components/ui/LoadingSpinner";
 import useGetInviteCode from "@/hooks/useGetInviteCode";
 
@@ -9,43 +9,42 @@ export default function SingleInvitePage() {
 
   if (isLoading) {
     return (
-      <Layout>
+      <DashboardLayout role="admin">
         <div className="flex justify-center items-center min-h-dvh">
           <Spinner />
         </div>
-      </Layout>
+      </DashboardLayout>
     );
   }
 
   if (isError) {
     return (
-      <Layout>
+      <DashboardLayout role="admin">
         <div className="flex justify-center items-center min-h-dvh">
-          <p className="text-red-600 font-medium text-lg text-center px-4">
+          <p className="text-red-600 font-medium text-lg">
             Error: This invite link has already been used or is invalid.
           </p>
         </div>
-      </Layout>
+      </DashboardLayout>
     );
   }
 
   return (
-    <Layout>
+    <DashboardLayout role="admin">
       <div className="flex justify-center items-center min-h-dvh">
-        <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md text-center border border-green-100">
-          <h2 className="text-xl font-semibold text-green-800 mb-3">
+        <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md text-center">
+          <h2 className="text-xl font-semibold text-gray-800 mb-3">
             Invitation Active
           </h2>
           <p className="text-gray-600">
             This invitation link is valid and ready to use.
           </p>
-
-          <div className="mt-6 bg-green-50 p-4 rounded-lg border border-green-200 text-green-800">
-            <p className="font-medium mb-1">Invite Code:</p>
+          <div className="mt-6 bg-blue-50 p-4 rounded-lg border border-blue-200 text-blue-800">
+            <p>Invite Code:</p>
             <p className="font-semibold break-all">{invite?.code}</p>
           </div>
         </div>
       </div>
-    </Layout>
+    </DashboardLayout>
   );
 }
